d6f700d3f1866c4eaed87782fe4f0fbed6951252
Vardan Melkonyan <vardan.melkonyan@tumo.org>
2021-04-25 17:02:33 +0400

    Add a new screen to display the game rules

---
+++ lib/screens/rules_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+class RulesSceeen extends StatefulWidget {
+  RulesSceeen({Key key}) : super(key: key);
+
+  @override
+  _RulesSceeenState createState() => _RulesSceeenState();
+}
+
+class _RulesSceeenState extends State<RulesSceeen> {
+  List<Widget> widgets = [];
+
+  @override
+  void initState() {
+    print("object");
+    readJson();
+    super.initState();
+  }
+
+  Future<void> readJson() async {
+    final String response =
+        await rootBundle.loadString('assets/Screens/rules.json');
+    final data = await json.decode(response);
+    setState(() {
+      data.forEach((key, value) {
+        widgets.add(SizedBox(
+          height: 40,
+        ));
+        widgets.add(Text(
+          key,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ));
+        widgets.add(SizedBox(
+          height: 10,
+        ));
+        widgets.add(Text(
+          value,
+          style: TextStyle(fontSize: 18),
+        ));
+      });
+      widgets.add(SizedBox(
+        height: 60,
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Rules of the game"),
+      ),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 100),
+        child: Column(
+          children: widgets,
+        ),
+      )),
+    );
+  }
+}
