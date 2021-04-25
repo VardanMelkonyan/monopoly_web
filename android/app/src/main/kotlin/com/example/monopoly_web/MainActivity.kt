5a737b01df20f39a0beb8711ec0f6ee88eb90d2b
Vardan Melkonyan <vardan.melkonyan@tumo.org>
2021-04-25 13:27:41 +0400

    First commit

---
+++ android/app/src/main/kotlin/com/example/monopoly_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.monopoly_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:monopoly_web/screens/home_screen.dart';
+
+void main() {
+  runApp(Monopoly());
+}
+
+class Monopoly extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Monopoly',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
