3501dc7239c0a3ac8c207be5707a8fec74171eb5
Vardan Melkonyan <vardan.melkonyan@tumo.org>
2021-04-27 00:16:52 +0400

    Fix Hero

--- lib/screens/number_of_players_screen.dart
+++ lib/screens/number_of_players_screen.dart
@@ -5,61 +5,55 @@ class NumberOfPlayersScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        child: Center(
-          child: Column(
-            children: [
-              Spacer(),
-              Hero(
-                tag: "PlayerCount",
-                child: Container(
-                  width: 400,
-                  //height: 210,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Color.fromARGB(255, 231, 76, 60),
-                  ),
-                  child: Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color.fromARGB(255, 195, 63, 49),
-                        ),
-                        height: 60,
-                        child: Center(
-                          child: Text(
-                            "Choose the number of players",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 24,
-                                color: Colors.white),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 150,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            NumberButton(2),
-                            NumberButton(3),
-                            NumberButton(4),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Spacer(),
-            ],
+      body: Hero(
+        tag: "PlayerCount",
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('background.jpg'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('background.jpg'),
-            fit: BoxFit.cover,
+          child: Center(
+            child: Container(
+              width: 400,
+              height: 210,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Color.fromARGB(255, 231, 76, 60),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color.fromARGB(255, 195, 63, 49),
+                    ),
+                    height: 60,
+                    child: Center(
+                      child: Text(
+                        "Choose the number of players",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 150,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        NumberButton(2),
+                        NumberButton(3),
+                        NumberButton(4),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
@@ -75,8 +69,10 @@ class NumberButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(
-              Color.fromARGB(255, 195, 63, 49))),
+        backgroundColor: MaterialStateProperty.all<Color>(
+          Color.fromARGB(255, 195, 63, 49),
+        ),
+      ),
       onPressed: () {
         print(number);
       },
@@ -84,7 +80,7 @@ class NumberButton extends StatelessWidget {
         child: Center(
           child: Text(
             "$number",
-            style: TextStyle(fontSize: 20),
+            style: TextStyle(fontSize: 24),
           ),
         ),
         width: 60,
